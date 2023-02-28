99277c2b51131b15a005680104f4c9dc4d344290
phiarchitect <phi@phiarchitect.com>
2023-02-28 08:35:23 -0800

    update script

--- root3.py
+++ root3.py
@@ -1,6 +1,4 @@
-from geometor.utils import *
-from geometor.model import *
-from geometor.render import *
+from geometor import *
 
 sp.init_printing()
 
@@ -8,154 +6,200 @@ BUILD = True
 ANALYZE = False
 
 PART1 = True
-PART2 = True
-PART3 = True
-PART4 = True
-if PART1:
-    print_log(f'\nPART1')
-    if PART2:
-        print_log('PART2')
-        if PART3:
-            print_log('PART3')
-            if PART4:
-                print_log('PART4')
+PART2 = False
+PART3 = False
+PART4 = False
 
-NAME = 'root3-'
-NAME += input(f'\nsession name: {NAME}')
-log_init(NAME)
+def main():
+    if PART1:
+        print_log(f'\nPART1')
+        if PART2:
+            print_log('PART2')
+            if PART3:
+                print_log('PART3')
+                if PART4:
+                    print_log('PART4')
 
-start_time = timer()
+    NAME = 'root3-'
+    NAME += input(f'\nsession name: {NAME}')
+    log_init(NAME)
 
-print_log(f'\nMODEL: {NAME}')
-# add starting points
-A, B = begin()
+    start_time = timer()
 
-# add baseline
-baseline = add_element(line(A, B))
+    print_log(f'\nMODEL: {NAME}')
 
-# add unit circles
-add_element(circle(A, B))
-add_element(circle(B, A))
- 
-C = pts[5]
-D = pts[4]
-#  O = pts[6]
+    # add starting points
+    start_time = timer()
 
-add_polygon(polygon_ids([0, 1, 5]))
+    M = Model()
+    # TODO: add label to Models
+    A = M.gen_point(0, 0, classes=['start'])
+    B = M.gen_point(1, 0, classes=['start'])
+    baseline = M.gen_line(A, B)
 
-add_element(line(A, D))
-add_element(line(A, C))
-add_element(line(B, C))
-add_element(line(B, D))
+    c1 = M.gen_circle(A, B)
+    C = M.points()[-1]
 
-add_polygon(polygon_ids([4, 6, 9]))
+    c2 = M.gen_circle(B, A)
+    D = M.points()[-3]
+    E = M.points()[-2]
+    F = M.points()[-1]
 
-add_element(line(D, C, classes=['bisector']))
-add_element(line(A, pts[9], classes=['bisector']))
-add_element(line(B, pts[6], classes=['bisector']))
-circumcenter = pts[14]
+    t1 = M.gen_polygon([A, B, E])
+    t2 = M.gen_polygon([A, B, F])
 
-# circumcircle
-add_element(circle(circumcenter, D, classes=['gold']))
+    len_pts = len(M.points())
+    bisector = M.gen_line(E, F, classes=['bisector'])
 
-add_element(circle(C, A))
-add_element(line(pts[6], pts[9]))
+    O = M.points()[len_pts]
 
-if PART1:
-    nine_ids = [4, 22, 19, 9, 21, 23, 6, 18, 20]
-    nine = add_polygon(polygon_ids(nine_ids, classes=['nine']))
+    len_pts = len(M.points())
+    l = M.gen_line(A, E)
+    G = M.points()[len_pts]
 
-    add_element(line(pts[20], pts[22], classes=['red']))
-    add_element(line(pts[19], pts[21], classes=['red']))
-    add_element(line(pts[23], pts[18], classes=['red']))
+    len_pts = len(M.points())
+    l = M.gen_line(B, E)
+    H = M.points()[len_pts]
 
-    if PART2:
-        pt_apex = D
-        add_element(line(pt_apex, pts[22], classes=['green']))
-        if PART3:
-            add_element(line(pt_apex, pts[19], classes=['set1']))
-            if PART4:
-                add_element(line(pt_apex, pts[21], classes=['set2']))
-                add_element(line(pt_apex, pts[23], classes=['set2']))
-            add_element(line(pt_apex, pts[18], classes=['set1']))
-        add_element(line(pt_apex, pts[20], classes=['green']))
+    t3 = M.gen_polygon([G, E, H])
+    
+    l = M.gen_line(A, F)
 
-        pt_apex = pts[6]
-        add_element(line(pt_apex, pts[18], classes=['green']))
-        if PART3:
-            add_element(line(pt_apex, pts[20], classes=['set1']))
-            if PART4:
-                add_element(line(pt_apex, pts[22], classes=['set2']))
-                add_element(line(pt_apex, pts[19], classes=['set2']))
-            add_element(line(pt_apex, pts[21], classes=['set1']))
-        add_element(line(pt_apex, pts[23], classes=['green']))
+    l = M.gen_line(B, F)
 
-        pt_apex = pts[9]
-        add_element(line(pt_apex, pts[21], classes=['green']))
-        if PART3:
-            add_element(line(pt_apex, pts[23], classes=['set1']))
-            if PART4:
-                add_element(line(pt_apex, pts[18], classes=['set2']))
-                add_element(line(pt_apex, pts[20], classes=['set2']))
-            add_element(line(pt_apex, pts[22], classes=['set1']))
-        add_element(line(pt_apex, pts[19], classes=['green']))
+    M.summary()
 
-model_summary(NAME, start_time)
-
-# ANALYZE ***************************
-if ANALYZE:
     print_log(f'\nANALYZE: {NAME}')
-    goldens, groups = analyze_model()
+    sections, sections_by_line = analyze_golden(M)
 
-    analyze_summary(NAME, start_time, goldens, groups)
 
-# PLOT *********************************
-print_log(f'\nPLOT: {NAME}')
-limx, limy = get_limits_from_points(pts, margin=.25)
-limx, limy = adjust_lims(limx, limy)
-bounds = set_bounds(limx, limy)
-print_log()
-print_log(f'limx: {limx}')
-print_log(f'limy: {limy}')
+    # PLOT *********************************
+    print_log(f'\nPLOT: {NAME}')
 
-#  plt.ion()
-fig, (ax, ax_btm) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [10, 1]})
-ax_btm.axis('off')
-ax.axis('off')
-ax.set_aspect('equal')
-plt.tight_layout()
+    fig, (ax, ax_btm) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [10, 1]})
+    plt.tight_layout()
 
-title = f'G E O M E T O R'
-fig.suptitle(title, fontdict={'color': '#960', 'size':'small'})
+    plot_model(NAME, ax, ax_btm, M)
 
-print_log('\nPlot Summary')
-xlabel = f'elements: {len(elements)} | points: {len(pts)}'
-ax_prep(ax, ax_btm, bounds, xlabel)
-plot_sequence(ax, history, bounds)
-snapshot(NAME, 'sequences/summary.png')
+    #  title = f'G E O M E T O R'
+    #  fig.suptitle(title, fontdict={'color': '#960', 'size':'small'})
 
-if BUILD:
-    print_log('\nPlot Build')
-    build_sequence(NAME, ax, ax_btm, history, bounds)
-
-if ANALYZE:
     print_log('\nPlot Goldens')
+    plot_sections(NAME, ax, ax_btm, sections)
 
-    bounds = get_bounds_from_sections(goldens)
-
-    plot_sections(NAME, ax, ax_btm, history, goldens, bounds)
+    plot_all_sections(NAME, ax, ax_btm, M, sections)
 
     print_log('\nPlot Golden Groups')
-    plot_all_groups(NAME, ax, ax_btm, history, groups, bounds)
+    groups = group_sections(sections)
+    plot_all_groups(NAME, ax, ax_btm, M, groups)
 
-    plot_all_sections(NAME, ax, ax_btm, history, goldens, bounds)
+    #  add_element(line(D, C, classes=['bisector']))
+    #  add_element(line(A, pts[9], classes=['bisector']))
+    #  add_element(line(B, pts[6], classes=['bisector']))
+    #  circumcenter = pts[14]
 
-    complete_summary(NAME, start_time, goldens, groups)
+    #  # circumcircle
+    #  add_element(circle(circumcenter, D, classes=['gold']))
 
-else:
-    model_summary(NAME, start_time)
+    #  add_element(circle(C, A))
+    #  add_element(line(pts[6], pts[9]))
 
+    #  if PART1:
+        #  nine_ids = [4, 22, 19, 9, 21, 23, 6, 18, 20]
+        #  nine = add_polygon(polygon_ids(nine_ids, classes=['nine']))
 
+        #  add_element(line(pts[20], pts[22], classes=['red']))
+        #  add_element(line(pts[19], pts[21], classes=['red']))
+        #  add_element(line(pts[23], pts[18], classes=['red']))
 
-plt.show()
+        #  if PART2:
+            #  pt_apex = D
+            #  add_element(line(pt_apex, pts[22], classes=['green']))
+            #  if PART3:
+                #  add_element(line(pt_apex, pts[19], classes=['set1']))
+                #  if PART4:
+                    #  add_element(line(pt_apex, pts[21], classes=['set2']))
+                    #  add_element(line(pt_apex, pts[23], classes=['set2']))
+                #  add_element(line(pt_apex, pts[18], classes=['set1']))
+            #  add_element(line(pt_apex, pts[20], classes=['green']))
+
+            #  pt_apex = pts[6]
+            #  add_element(line(pt_apex, pts[18], classes=['green']))
+            #  if PART3:
+                #  add_element(line(pt_apex, pts[20], classes=['set1']))
+                #  if PART4:
+                    #  add_element(line(pt_apex, pts[22], classes=['set2']))
+                    #  add_element(line(pt_apex, pts[19], classes=['set2']))
+                #  add_element(line(pt_apex, pts[21], classes=['set1']))
+            #  add_element(line(pt_apex, pts[23], classes=['green']))
+
+            #  pt_apex = pts[9]
+            #  add_element(line(pt_apex, pts[21], classes=['green']))
+            #  if PART3:
+                #  add_element(line(pt_apex, pts[23], classes=['set1']))
+                #  if PART4:
+                    #  add_element(line(pt_apex, pts[18], classes=['set2']))
+                    #  add_element(line(pt_apex, pts[20], classes=['set2']))
+                #  add_element(line(pt_apex, pts[22], classes=['set1']))
+            #  add_element(line(pt_apex, pts[19], classes=['green']))
+
+    #  model_summary(NAME, start_time)
+
+    #  # ANALYZE ***************************
+    #  if ANALYZE:
+        #  print_log(f'\nANALYZE: {NAME}')
+        #  goldens, groups = analyze_model()
+
+        #  analyze_summary(NAME, start_time, goldens, groups)
+
+    #  # PLOT *********************************
+    #  print_log(f'\nPLOT: {NAME}')
+    #  limx, limy = get_limits_from_points(pts, margin=.25)
+    #  limx, limy = adjust_lims(limx, limy)
+    #  bounds = set_bounds(limx, limy)
+    #  print_log()
+    #  print_log(f'limx: {limx}')
+    #  print_log(f'limy: {limy}')
+
+    #  #  plt.ion()
+    #  fig, (ax, ax_btm) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [10, 1]})
+    #  ax_btm.axis('off')
+    #  ax.axis('off')
+    #  ax.set_aspect('equal')
+    #  plt.tight_layout()
+
+    #  title = f'G E O M E T O R'
+    #  fig.suptitle(title, fontdict={'color': '#960', 'size':'small'})
+
+    #  print_log('\nPlot Summary')
+    #  xlabel = f'elements: {len(elements)} | points: {len(pts)}'
+    #  ax_prep(ax, ax_btm, bounds, xlabel)
+    #  plot_sequence(ax, history, bounds)
+    #  snapshot(NAME, 'sequences/summary.png')
+
+    #  if BUILD:
+        #  print_log('\nPlot Build')
+        #  build_sequence(NAME, ax, ax_btm, history, bounds)
+
+    #  if ANALYZE:
+        #  print_log('\nPlot Goldens')
+
+        #  bounds = get_bounds_from_sections(goldens)
+
+        #  plot_sections(NAME, ax, ax_btm, history, goldens, bounds)
+
+        #  print_log('\nPlot Golden Groups')
+        #  plot_all_groups(NAME, ax, ax_btm, history, groups, bounds)
+
+        #  plot_all_sections(NAME, ax, ax_btm, history, goldens, bounds)
+
+        #  complete_summary(NAME, start_time, goldens, groups)
+
+    #  else:
+        #  model_summary(NAME, start_time)
+
+    #  plt.show()
+
+if __name__ == "__main__":
+    main()
 
--- root5.py
+++ root5.py
@@ -13,138 +13,200 @@ PART1 = True
 PART2 = True
 PART3 = True
 PART4 = False
-if PART1:
-    print_log(f'\nPART1')
-    if PART2:
-        print_log('PART2')
-        if PART3:
-            print_log('PART3')
 
-NAME = 'root5-'
-NAME += input(f'\nsession name: {NAME}')
-log_init(NAME)
-start_time = timer()
+def main():
+    """TODO: Docstring for main.
+    :returns: TODO
 
-print_log(f'\nMODEL: {NAME}')
-# add starting points
-A, B = begin()
+    """
 
-# find perps for square
-bisector(A, B)
-bisector(A, pts[3])
-bisector(B, pts[2])
+    if PART1:
+        print_log(f'\nPART1')
+        if PART2:
+            print_log('PART2')
+            if PART3:
+                print_log('PART3')
 
-# squares
-add_element(line(pts[18], pts[32]))
-add_element(line(pts[17], pts[31]))
-add_polygon(polygon_ids([0, 1, 18, 32]))
-add_polygon(polygon_ids([0, 1, 17, 31]))
+    NAME = 'root5-'
+    NAME += input(f'\nsession name: {NAME}')
+    log_init(NAME)
+    start_time = timer()
 
-# golden circle
-c = add_element(circle(pts[6], pts[18], classes=['gold']))
-goA = pts[53]
-goB = pts[54]
+    M = Model()
+    # TODO: add label to Models
+    A = M.gen_point(0, 0, classes=['start'])
+    B = M.gen_point(1, 0, classes=['start'])
+    baseline = M.gen_line(A, B)
 
-if PART1:
+    c1 = M.gen_circle(A, B)
+    C = M.points()[-1]
+
+    c2 = M.gen_circle(B, A)
+    D = M.points()[-3]
+    E = M.points()[-2]
+    F = M.points()[-1]
+
+    len_pts = len(M.points())
+    bisector = M.gen_line(E, F, classes=['bisector'])
+
+    O = M.points()[len_pts]
+    
+    c = M.gen_circle(B, C)
+    c = M.gen_circle(C, B)
+    
+    G = M.points()[-2]
+    H = M.points()[-1]
+    pts_len = len(M.points())
+    A_perp = M.gen_line(G, H)
+    new_pts = M.points()[pts_len:]
+    print(new_pts)
+
+    Snw = new_pts[0]
+    Ssw = new_pts[1]
+
+    c = M.gen_circle(A, D)
+    c = M.gen_circle(D, A)
+    
+    I = M.points()[-2]
+    J = M.points()[-1]
+    pts_len = len(M.points())
+    B_perp = M.gen_line(I, J)
+    new_pts = M.points()[pts_len:]
+
+    Sne = new_pts[0]
+    Sse = new_pts[1]
+
+    s1 = M.gen_polygon([A, B, Sne, Snw])
+    l1 = M.gen_line(Sne, Snw)
+
+    s2 = M.gen_polygon([A, B, Sse, Ssw])
+    l2 = M.gen_line(Sse, Ssw)
+
+    #  c = M.gen_circle(Snw, A )
+    #  c = M.gen_circle(Ssw, A )
+
+    #  # golden circle
+    pts_len = len(M.points())
+    c = M.gen_circle(O, Snw, classes=['gold'])
+    new_pts = M.points()[pts_len:]
+
+    goA = new_pts[0]
+    goB = new_pts[1]
+
     # outer goldens
-    el = add_element(circle(A, goB, classes=['gold']))
-    el = add_element(circle(B, goA, classes=['gold']))
+    c = M.gen_circle(A, goB, classes=['gold'])
+    c = M.gen_circle(B, goA, classes=['gold'])
 
-    pentagon = polygon_ids([0, 1, 62, 64, 81])
-    add_polygon(pentagon)
-    pentagon = polygon_ids([0, 1, 63, 65, 82])
-    add_polygon(pentagon)
+    #  pentagon = polygon_ids([0, 1, 62, 64, 81])
+    #  add_polygon(pentagon)
+    #  pentagon = polygon_ids([0, 1, 63, 65, 82])
+    #  add_polygon(pentagon)
 
-    if PART2:
-        # inner goldens
-        add_element(circle(A, goA, classes=['gold']))
-        add_element(circle(B, goB, classes=['gold']))
+    print(M)
+    M.summary()
 
-        # half unit circle
-        add_element(circle(pts[6], A))
+    # PLOT *********************************
+    print_log(f'\nPLOT: {NAME}')
 
-        # diagonals
-        add_element(line(pts[18], pts[31], classes=['green']))
-        add_element(line(pts[17], pts[32], classes=['green']))
+    fig, (ax, ax_btm) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [10, 1]})
+    plt.tight_layout()
 
-        if PART3:
-
-            add_element(line(pts[18], goA, classes=['set1']))
-            add_element(line(pts[17], goA, classes=['set1']))
-
-            add_element(line(pts[18], goB, classes=['set2']))
-            add_element(line(pts[17], goB, classes=['set2']))
-
-            add_element(line(pts[31], goA, classes=['set2']))
-            add_element(line(pts[32], goA, classes=['set2']))
-
-            add_element(line(pts[31], goB, classes=['set1']))
-            add_element(line(pts[32], goB, classes=['set1']))
-
-            # cross
-            add_element(line(pts[293], pts[292]))
-            add_element(line(pts[293], pts[291]))
-
-            add_element(line(pts[294], pts[292]))
-            add_element(line(pts[294], pts[291]))
+    plot_model(NAME, ax, ax_btm, M)
 
 
-model_summary(NAME, start_time)
+    #  if PART1:
 
-# ANALYZE ***************************
-if ANALYZE:
-    print_log(f'\nANALYZE: {NAME}')
-    goldens, groups = analyze_model()
+        #  if PART2:
+            #  # inner goldens
+            #  add_element(circle(A, goA, classes=['gold']))
+            #  add_element(circle(B, goB, classes=['gold']))
 
-    analyze_summary(NAME, start_time, goldens, groups)
+            #  # half unit circle
+            #  add_element(circle(pts[6], A))
 
-# PLOT *********************************
-print_log(f'\nPLOT: {NAME}')
-limx, limy = get_limits_from_points(pts, margin=.25)
-limx, limy = adjust_lims(limx, limy)
-bounds = set_bounds(limx, limy)
-print_log()
-print_log(f'limx: {limx}')
-print_log(f'limy: {limy}')
+            #  # diagonals
+            #  add_element(line(pts[18], pts[31], classes=['green']))
+            #  add_element(line(pts[17], pts[32], classes=['green']))
 
-#  plt.ion()
-fig, (ax, ax_btm) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [10, 1]})
-ax_btm.axis('off')
-ax.axis('off')
-ax.set_aspect('equal')
-plt.tight_layout()
+            #  if PART3:
 
-title = f'G E O M E T O R'
-fig.suptitle(title, fontdict={'color': '#960', 'size':'small'})
+                #  add_element(line(pts[18], goA, classes=['set1']))
+                #  add_element(line(pts[17], goA, classes=['set1']))
 
-print_log('\nPlot Summary')
-xlabel = f'elements: {len(elements)} | points: {len(pts)}'
-ax_prep(ax, ax_btm, bounds, xlabel)
-plot_sequence(ax, history, bounds)
-snapshot(NAME, 'sequences/summary.png')
+                #  add_element(line(pts[18], goB, classes=['set2']))
+                #  add_element(line(pts[17], goB, classes=['set2']))
 
-if BUILD:
-    print_log('\nPlot Build')
-    build_sequence(NAME, ax, ax_btm, history, bounds)
+                #  add_element(line(pts[31], goA, classes=['set2']))
+                #  add_element(line(pts[32], goA, classes=['set2']))
 
-if ANALYZE:
-    print_log('\nPlot Goldens')
+                #  add_element(line(pts[31], goB, classes=['set1']))
+                #  add_element(line(pts[32], goB, classes=['set1']))
 
-    bounds = get_bounds_from_sections(goldens)
+                #  # cross
+                #  add_element(line(pts[293], pts[292]))
+                #  add_element(line(pts[293], pts[291]))
 
-    plot_sections(NAME, ax, ax_btm, history, goldens, bounds)
-
-    print_log('\nPlot Golden Groups')
-    plot_all_groups(NAME, ax, ax_btm, history, groups, bounds)
-
-    plot_all_sections(NAME, ax, ax_btm, history, goldens, bounds)
-
-    complete_summary(NAME, start_time, goldens, groups)
-
-else:
-    model_summary(NAME, start_time)
+                #  add_element(line(pts[294], pts[292]))
+                #  add_element(line(pts[294], pts[291]))
 
 
+    #  model_summary(NAME, start_time)
 
-plt.show()
+    #  # ANALYZE ***************************
+    #  if ANALYZE:
+        #  print_log(f'\nANALYZE: {NAME}')
+        #  goldens, groups = analyze_model()
 
+        #  analyze_summary(NAME, start_time, goldens, groups)
+
+    #  # PLOT *********************************
+    #  print_log(f'\nPLOT: {NAME}')
+    #  limx, limy = get_limits_from_points(pts, margin=.25)
+    #  limx, limy = adjust_lims(limx, limy)
+    #  bounds = set_bounds(limx, limy)
+    #  print_log()
+    #  print_log(f'limx: {limx}')
+    #  print_log(f'limy: {limy}')
+
+    #  #  plt.ion()
+    #  fig, (ax, ax_btm) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [10, 1]})
+    #  ax_btm.axis('off')
+    #  ax.axis('off')
+    #  ax.set_aspect('equal')
+    #  plt.tight_layout()
+
+    #  title = f'G E O M E T O R'
+    #  fig.suptitle(title, fontdict={'color': '#960', 'size':'small'})
+
+    #  print_log('\nPlot Summary')
+    #  xlabel = f'elements: {len(elements)} | points: {len(pts)}'
+    #  ax_prep(ax, ax_btm, bounds, xlabel)
+    #  plot_sequence(ax, history, bounds)
+    #  snapshot(NAME, 'sequences/summary.png')
+
+    #  if BUILD:
+        #  print_log('\nPlot Build')
+        #  build_sequence(NAME, ax, ax_btm, history, bounds)
+
+    #  if ANALYZE:
+        #  print_log('\nPlot Goldens')
+
+        #  bounds = get_bounds_from_sections(goldens)
+
+        #  plot_sections(NAME, ax, ax_btm, history, goldens, bounds)
+
+        #  print_log('\nPlot Golden Groups')
+        #  plot_all_groups(NAME, ax, ax_btm, history, groups, bounds)
+
+        #  plot_all_sections(NAME, ax, ax_btm, history, goldens, bounds)
+
+        #  complete_summary(NAME, start_time, goldens, groups)
+
+    #  else:
+        #  model_summary(NAME, start_time)
+
+
+    #  plt.show()
+
+if __name__ == "__main__":
+    main()
